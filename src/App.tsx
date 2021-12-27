e05fdbf31eb3a746613344a4e5eed98e8829711c
tigrankochinyan <tigrankochinyan24@gmail.com>
2021-12-27 10:49:34 +0400

    add some componnets and main page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import LeftNavigation from './components/LeftNavigation';
 import RightSide from './components/RightSide';
 import ContainerMy from './components/Container';
+import Main from './pages/Main';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css';
 
@@ -13,14 +14,15 @@ function App() {
       <Router>
         <Header />
         <Routes>
-          <Route path="/" element={<LeftNavigation />}>
-            <Route index element={<ContainerMy />} />
-            <Route path="teams" element={<LeftNavigation />}>
+          <Route path="/" element={<Main />}>
+            {/* <Route path="folders" element={<LeftNavigation />}>
               <Route path=":teamId" element={<RightSide />} />
               <Route path="new" element={<RightSide />} />
               <Route index element={<>s</>} />
+            </Route> */}
+            <Route path="/folders">
+              <Route path="*" element={<Main />} />
             </Route>
-            <Route path="folders" element={<LeftNavigation />} />
           </Route>
         </Routes>
       </Router>
