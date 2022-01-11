e1cfa1e038891576c55f4a2e0d35bd8cfc1ec689
tigrankochinyan <tigrankochinyan24@gmail.com>
2022-01-11 18:57:59 +0400

    add ModalForm for file, delete button

--- src/components/RightSide/ActionsButtons/ActionsButtons.tsx
+++ src/components/RightSide/ActionsButtons/ActionsButtons.tsx
@@ -1,13 +1,14 @@
+import { useDispatch } from 'react-redux';
 import { history } from '../../../redux/reducers';
 
+// import CreateNewFolderIcon from '@mui/icons-material/CreateNewFolder';
+// import FeedIcon from '@mui/icons-material/Feed';
 import Button from '@mui/material/Button';
-
-import CreateNewFolderIcon from '@mui/icons-material/CreateNewFolder';
-import FeedIcon from '@mui/icons-material/Feed';
-import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
 import Stack from '@mui/material/Stack';
-
 import ModalForm from '../ModalForm';
+
+import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 import styles from './index.module.scss';
 
@@ -15,18 +16,26 @@ interface ActionButtonsProps {
     parentId: number;
 }
 
-const ActionBttons = ({parentId}) => {
-    // console.log('parentId', parentId);
+const ActionBttons = ({id, parentId, disableActions}) => {
+    const dispatch = useDispatch();
+    const deleteFile = (id) => {
+        dispatch({type: 'DELETE_ITEM', payload: {id}});
+        history.goBack();
+    }
     
     return <Stack direction="row" spacing={2}>
-        <ModalForm />
-        <Button 
+        <Button
             variant="contained" 
-            onClick={() => history.push(`/${parentId ? parentId : ''}`)}
+            onClick={() => history.goBack()}
             startIcon={<KeyboardBackspaceIcon />}
         />
-        <Button variant="contained" endIcon={<CreateNewFolderIcon />} />
-        <Button variant="contained" endIcon={<FeedIcon />} />
+        <Button 
+            variant="contained" 
+            onClick={() => deleteFile(id)}
+            startIcon={<DeleteIcon />}
+        />
+        <ModalForm disabled={disableActions} type="folder"/>
+        <ModalForm disabled={disableActions} type="file"/>
     </Stack>
 }
 
